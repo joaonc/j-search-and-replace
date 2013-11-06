1fbe95a6dbe1bea0ed8f149cdd7d2bcd6df7d4b7
joaonc@gmail.com <joaonc@gmail.com@4aba3830-8a5b-77d7-608e-5fc8d1c71f61>
2013-11-06 02:55:44 +0000

    More UI settings.

--- trunk/JSearchAndReplace/JSearchAndReplace/FormJSearchAndReplace.Designer.cs
+++ trunk/JSearchAndReplace/JSearchAndReplace/FormJSearchAndReplace.Designer.cs
@@ -34,17 +34,28 @@
             this.buttonFileOut = new System.Windows.Forms.Button();
             this.buttonGo = new System.Windows.Forms.Button();
             this.textBoxInstructions = new System.Windows.Forms.TextBox();
+            this.labelFileIn = new System.Windows.Forms.Label();
+            this.labelFileOut = new System.Windows.Forms.Label();
+            this.labelSearchAndReplaceText = new System.Windows.Forms.Label();
+            this.radioButtonSearchAndReplaceFile = new System.Windows.Forms.RadioButton();
+            this.radioButtonSearchAndReplaceSet = new System.Windows.Forms.RadioButton();
+            this.textBoxFileSearchAndReplace = new System.Windows.Forms.TextBox();
+            this.comboBoxSearchAndReplaceSet = new System.Windows.Forms.ComboBox();
+            this.buttonWindowsExplorerIntegration = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBoxFileIn
             // 
+            this.textBoxFileIn.AllowDrop = true;
             this.textBoxFileIn.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxFileIn.Location = new System.Drawing.Point(13, 34);
+            this.textBoxFileIn.Location = new System.Drawing.Point(71, 34);
             this.textBoxFileIn.Name = "textBoxFileIn";
-            this.textBoxFileIn.Size = new System.Drawing.Size(384, 20);
+            this.textBoxFileIn.Size = new System.Drawing.Size(326, 20);
             this.textBoxFileIn.TabIndex = 0;
             this.textBoxFileIn.Text = "TestSearchAndReplace.srt";
+            this.textBoxFileIn.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBoxFileIn_DragDrop);
+            this.textBoxFileIn.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBoxFileIn_DragEnter);
             // 
             // buttonFileIn
             // 
@@ -60,9 +71,9 @@
             // 
             this.textBoxFileOut.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxFileOut.Location = new System.Drawing.Point(13, 61);
+            this.textBoxFileOut.Location = new System.Drawing.Point(71, 61);
             this.textBoxFileOut.Name = "textBoxFileOut";
-            this.textBoxFileOut.Size = new System.Drawing.Size(384, 20);
+            this.textBoxFileOut.Size = new System.Drawing.Size(326, 20);
             this.textBoxFileOut.TabIndex = 2;
             this.textBoxFileOut.Text = "<n>.out<e>";
             // 
@@ -79,7 +90,7 @@
             // buttonGo
             // 
             this.buttonGo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonGo.Location = new System.Drawing.Point(403, 117);
+            this.buttonGo.Location = new System.Drawing.Point(403, 229);
             this.buttonGo.Name = "buttonGo";
             this.buttonGo.Size = new System.Drawing.Size(75, 23);
             this.buttonGo.TabIndex = 4;
@@ -98,11 +109,100 @@
             this.textBoxInstructions.TabIndex = 5;
             this.textBoxInstructions.Text = "<n> Filename\r\n<e> Extension\r\n<s:\"ab\" r:\"cd\"> Search and replace";
             // 
+            // labelFileIn
+            // 
+            this.labelFileIn.AutoSize = true;
+            this.labelFileIn.Location = new System.Drawing.Point(12, 37);
+            this.labelFileIn.Name = "labelFileIn";
+            this.labelFileIn.Size = new System.Drawing.Size(50, 13);
+            this.labelFileIn.TabIndex = 6;
+            this.labelFileIn.Text = "Input File";
+            // 
+            // labelFileOut
+            // 
+            this.labelFileOut.AutoSize = true;
+            this.labelFileOut.Location = new System.Drawing.Point(12, 64);
+            this.labelFileOut.Name = "labelFileOut";
+            this.labelFileOut.Size = new System.Drawing.Size(58, 13);
+            this.labelFileOut.TabIndex = 7;
+            this.labelFileOut.Text = "Output File";
+            // 
+            // labelSearchAndReplaceText
+            // 
+            this.labelSearchAndReplaceText.AutoSize = true;
+            this.labelSearchAndReplaceText.Location = new System.Drawing.Point(13, 147);
+            this.labelSearchAndReplaceText.Name = "labelSearchAndReplaceText";
+            this.labelSearchAndReplaceText.Size = new System.Drawing.Size(120, 13);
+            this.labelSearchAndReplaceText.TabIndex = 8;
+            this.labelSearchAndReplaceText.Text = "Search and replace text";
+            // 
+            // radioButtonSearchAndReplaceFile
+            // 
+            this.radioButtonSearchAndReplaceFile.AutoSize = true;
+            this.radioButtonSearchAndReplaceFile.Location = new System.Drawing.Point(13, 164);
+            this.radioButtonSearchAndReplaceFile.Name = "radioButtonSearchAndReplaceFile";
+            this.radioButtonSearchAndReplaceFile.Size = new System.Drawing.Size(67, 17);
+            this.radioButtonSearchAndReplaceFile.TabIndex = 9;
+            this.radioButtonSearchAndReplaceFile.Text = "From File";
+            this.radioButtonSearchAndReplaceFile.UseVisualStyleBackColor = true;
+            this.radioButtonSearchAndReplaceFile.CheckedChanged += new System.EventHandler(this.radioButtonSearchAndReplaceFile_CheckedChanged);
+            // 
+            // radioButtonSearchAndReplaceSet
+            // 
+            this.radioButtonSearchAndReplaceSet.AutoSize = true;
+            this.radioButtonSearchAndReplaceSet.Checked = true;
+            this.radioButtonSearchAndReplaceSet.Location = new System.Drawing.Point(13, 188);
+            this.radioButtonSearchAndReplaceSet.Name = "radioButtonSearchAndReplaceSet";
+            this.radioButtonSearchAndReplaceSet.Size = new System.Drawing.Size(80, 17);
+            this.radioButtonSearchAndReplaceSet.TabIndex = 10;
+            this.radioButtonSearchAndReplaceSet.TabStop = true;
+            this.radioButtonSearchAndReplaceSet.Text = "Existing Set";
+            this.radioButtonSearchAndReplaceSet.UseVisualStyleBackColor = true;
+            this.radioButtonSearchAndReplaceSet.CheckedChanged += new System.EventHandler(this.radioButtonSearchAndReplaceSet_CheckedChanged);
+            // 
+            // textBoxFileSearchAndReplace
+            // 
+            this.textBoxFileSearchAndReplace.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFileSearchAndReplace.Location = new System.Drawing.Point(100, 161);
+            this.textBoxFileSearchAndReplace.Name = "textBoxFileSearchAndReplace";
+            this.textBoxFileSearchAndReplace.Size = new System.Drawing.Size(297, 20);
+            this.textBoxFileSearchAndReplace.TabIndex = 11;
+            this.textBoxFileSearchAndReplace.DragDrop += new System.Windows.Forms.DragEventHandler(this.textBoxFileSearchAndReplace_DragDrop);
+            this.textBoxFileSearchAndReplace.DragEnter += new System.Windows.Forms.DragEventHandler(this.textBoxFileSearchAndReplace_DragEnter);
+            // 
+            // comboBoxSearchAndReplaceSet
+            // 
+            this.comboBoxSearchAndReplaceSet.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxSearchAndReplaceSet.FormattingEnabled = true;
+            this.comboBoxSearchAndReplaceSet.Location = new System.Drawing.Point(100, 188);
+            this.comboBoxSearchAndReplaceSet.Name = "comboBoxSearchAndReplaceSet";
+            this.comboBoxSearchAndReplaceSet.Size = new System.Drawing.Size(213, 21);
+            this.comboBoxSearchAndReplaceSet.TabIndex = 12;
+            // 
+            // buttonWindowsExplorerIntegration
+            // 
+            this.buttonWindowsExplorerIntegration.Location = new System.Drawing.Point(12, 229);
+            this.buttonWindowsExplorerIntegration.Name = "buttonWindowsExplorerIntegration";
+            this.buttonWindowsExplorerIntegration.Size = new System.Drawing.Size(121, 23);
+            this.buttonWindowsExplorerIntegration.TabIndex = 13;
+            this.buttonWindowsExplorerIntegration.Text = "Explorer Integration";
+            this.buttonWindowsExplorerIntegration.UseVisualStyleBackColor = true;
+            this.buttonWindowsExplorerIntegration.Click += new System.EventHandler(this.buttonWindowsExplorerIntegration_Click);
+            // 
             // FormJSearchAndReplace
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(490, 152);
+            this.ClientSize = new System.Drawing.Size(490, 264);
+            this.Controls.Add(this.buttonWindowsExplorerIntegration);
+            this.Controls.Add(this.comboBoxSearchAndReplaceSet);
+            this.Controls.Add(this.textBoxFileSearchAndReplace);
+            this.Controls.Add(this.radioButtonSearchAndReplaceSet);
+            this.Controls.Add(this.radioButtonSearchAndReplaceFile);
+            this.Controls.Add(this.labelSearchAndReplaceText);
+            this.Controls.Add(this.labelFileOut);
+            this.Controls.Add(this.labelFileIn);
             this.Controls.Add(this.textBoxInstructions);
             this.Controls.Add(this.buttonGo);
             this.Controls.Add(this.buttonFileOut);
@@ -112,6 +212,7 @@
             this.Name = "FormJSearchAndReplace";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "J Search and Replace";
+            this.Load += new System.EventHandler(this.FormJSearchAndReplace_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -125,6 +226,14 @@
         private System.Windows.Forms.Button buttonFileOut;
         private System.Windows.Forms.Button buttonGo;
         private System.Windows.Forms.TextBox textBoxInstructions;
+        private System.Windows.Forms.Label labelFileIn;
+        private System.Windows.Forms.Label labelFileOut;
+        private System.Windows.Forms.Label labelSearchAndReplaceText;
+        private System.Windows.Forms.RadioButton radioButtonSearchAndReplaceFile;
+        private System.Windows.Forms.RadioButton radioButtonSearchAndReplaceSet;
+        private System.Windows.Forms.TextBox textBoxFileSearchAndReplace;
+        private System.Windows.Forms.ComboBox comboBoxSearchAndReplaceSet;
+        private System.Windows.Forms.Button buttonWindowsExplorerIntegration;
     }
 }
 
---
+++ trunk/JSearchAndReplace/JSearchAndReplace/FormWindowsExplorerIntegration.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace JSearchAndReplace
+{
+    partial class FormWindowsExplorerIntegration
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonClose = new System.Windows.Forms.Button();
+            this.checkBoxEnableRightClick = new System.Windows.Forms.CheckBox();
+            this.radioButtonAllFiles = new System.Windows.Forms.RadioButton();
+            this.radioButtonFileExtensions = new System.Windows.Forms.RadioButton();
+            this.textBoxFileExtensions = new System.Windows.Forms.TextBox();
+            this.groupBoxFiles = new System.Windows.Forms.GroupBox();
+            this.buttonOk = new System.Windows.Forms.Button();
+            this.groupBoxSearchAndReplace = new System.Windows.Forms.GroupBox();
+            this.groupBoxFiles.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonClose
+            // 
+            this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonClose.Location = new System.Drawing.Point(289, 286);
+            this.buttonClose.Name = "buttonClose";
+            this.buttonClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonClose.TabIndex = 0;
+            this.buttonClose.Text = "Close";
+            this.buttonClose.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxEnableRightClick
+            // 
+            this.checkBoxEnableRightClick.AutoSize = true;
+            this.checkBoxEnableRightClick.Location = new System.Drawing.Point(12, 12);
+            this.checkBoxEnableRightClick.Name = "checkBoxEnableRightClick";
+            this.checkBoxEnableRightClick.Size = new System.Drawing.Size(113, 17);
+            this.checkBoxEnableRightClick.TabIndex = 2;
+            this.checkBoxEnableRightClick.Text = "Enable Right Click";
+            this.checkBoxEnableRightClick.UseVisualStyleBackColor = true;
+            this.checkBoxEnableRightClick.CheckedChanged += new System.EventHandler(this.checkBoxEnableRightClick_CheckedChanged);
+            // 
+            // radioButtonAllFiles
+            // 
+            this.radioButtonAllFiles.AutoSize = true;
+            this.radioButtonAllFiles.Location = new System.Drawing.Point(13, 19);
+            this.radioButtonAllFiles.Name = "radioButtonAllFiles";
+            this.radioButtonAllFiles.Size = new System.Drawing.Size(60, 17);
+            this.radioButtonAllFiles.TabIndex = 3;
+            this.radioButtonAllFiles.Text = "All Files";
+            this.radioButtonAllFiles.UseVisualStyleBackColor = true;
+            this.radioButtonAllFiles.CheckedChanged += new System.EventHandler(this.radioButtonAllFiles_CheckedChanged);
+            // 
+            // radioButtonFileExtensions
+            // 
+            this.radioButtonFileExtensions.AutoSize = true;
+            this.radioButtonFileExtensions.Checked = true;
+            this.radioButtonFileExtensions.Location = new System.Drawing.Point(13, 42);
+            this.radioButtonFileExtensions.Name = "radioButtonFileExtensions";
+            this.radioButtonFileExtensions.Size = new System.Drawing.Size(109, 17);
+            this.radioButtonFileExtensions.TabIndex = 4;
+            this.radioButtonFileExtensions.TabStop = true;
+            this.radioButtonFileExtensions.Text = "These Extensions";
+            this.radioButtonFileExtensions.UseVisualStyleBackColor = true;
+            this.radioButtonFileExtensions.CheckedChanged += new System.EventHandler(this.radioButtonFileExtensions_CheckedChanged);
+            // 
+            // textBoxFileExtensions
+            // 
+            this.textBoxFileExtensions.Location = new System.Drawing.Point(128, 41);
+            this.textBoxFileExtensions.Multiline = true;
+            this.textBoxFileExtensions.Name = "textBoxFileExtensions";
+            this.textBoxFileExtensions.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxFileExtensions.Size = new System.Drawing.Size(100, 51);
+            this.textBoxFileExtensions.TabIndex = 5;
+            this.textBoxFileExtensions.Text = "*.srt";
+            // 
+            // groupBoxFiles
+            // 
+            this.groupBoxFiles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBoxFiles.Controls.Add(this.textBoxFileExtensions);
+            this.groupBoxFiles.Controls.Add(this.radioButtonFileExtensions);
+            this.groupBoxFiles.Controls.Add(this.radioButtonAllFiles);
+            this.groupBoxFiles.Location = new System.Drawing.Point(12, 35);
+            this.groupBoxFiles.Name = "groupBoxFiles";
+            this.groupBoxFiles.Size = new System.Drawing.Size(352, 103);
+            this.groupBoxFiles.TabIndex = 6;
+            this.groupBoxFiles.TabStop = false;
+            this.groupBoxFiles.Text = "File Extensions";
+            // 
+            // buttonOk
+            // 
+            this.buttonOk.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOk.Location = new System.Drawing.Point(208, 286);
+            this.buttonOk.Name = "buttonOk";
+            this.buttonOk.Size = new System.Drawing.Size(75, 23);
+            this.buttonOk.TabIndex = 7;
+            this.buttonOk.Text = "OK";
+            this.buttonOk.UseVisualStyleBackColor = true;
+            this.buttonOk.Click += new System.EventHandler(this.buttonOk_Click);
+            // 
+            // groupBoxSearchAndReplace
+            // 
+            this.groupBoxSearchAndReplace.Location = new System.Drawing.Point(13, 145);
+            this.groupBoxSearchAndReplace.Name = "groupBoxSearchAndReplace";
+            this.groupBoxSearchAndReplace.Size = new System.Drawing.Size(351, 100);
+            this.groupBoxSearchAndReplace.TabIndex = 8;
+            this.groupBoxSearchAndReplace.TabStop = false;
+            this.groupBoxSearchAndReplace.Text = "Search and Replace";
+            // 
+            // FormWindowsExplorerIntegration
+            // 
+            this.AcceptButton = this.buttonOk;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonClose;
+            this.ClientSize = new System.Drawing.Size(376, 321);
+            this.Controls.Add(this.groupBoxSearchAndReplace);
+            this.Controls.Add(this.buttonOk);
+            this.Controls.Add(this.checkBoxEnableRightClick);
+            this.Controls.Add(this.buttonClose);
+            this.Controls.Add(this.groupBoxFiles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormWindowsExplorerIntegration";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Windows Explorer Integration";
+            this.Load += new System.EventHandler(this.FormWindowsExplorerIntegration_Load);
+            this.groupBoxFiles.ResumeLayout(false);
+            this.groupBoxFiles.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.CheckBox checkBoxEnableRightClick;
+        private System.Windows.Forms.RadioButton radioButtonAllFiles;
+        private System.Windows.Forms.RadioButton radioButtonFileExtensions;
+        private System.Windows.Forms.TextBox textBoxFileExtensions;
+        private System.Windows.Forms.GroupBox groupBoxFiles;
+        private System.Windows.Forms.Button buttonOk;
+        private System.Windows.Forms.GroupBox groupBoxSearchAndReplace;
+    }
+}
