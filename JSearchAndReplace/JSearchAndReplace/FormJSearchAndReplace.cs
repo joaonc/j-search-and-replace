638cab36153b10566989160aebe50875fc04e506
joaonc <joaonc@gmail.com>
2013-11-06 02:55:44 +0000

    More UI settings.

--- JSearchAndReplace/JSearchAndReplace/FormJSearchAndReplace.cs
+++ JSearchAndReplace/JSearchAndReplace/FormJSearchAndReplace.cs
@@ -17,6 +17,18 @@ namespace JSearchAndReplace
             InitializeComponent();
         }
 
+        private void FormJSearchAndReplace_Load(object sender, EventArgs e)
+        {
+            comboBoxSearchAndReplaceSet.Items.Add("Remove Diacritics");
+            comboBoxSearchAndReplaceSet.SelectedIndex = 0;
+
+            UpdateUI();
+        }
+
+        /// <summary>
+        /// Gets all the parameters from the UI.
+        /// </summary>
+        /// <returns></returns>
         SearchAndReplaceParameters GetSearchAndReplaceParameters()
         {
             SearchAndReplaceParameters searchAndReplaceParameters = new SearchAndReplaceParameters();
@@ -29,11 +41,57 @@ namespace JSearchAndReplace
             return searchAndReplaceParameters;
         }
 
+        private void UpdateUI()
+        {
+            textBoxFileSearchAndReplace.Enabled = radioButtonSearchAndReplaceFile.Checked;
+            comboBoxSearchAndReplaceSet.Enabled = radioButtonSearchAndReplaceSet.Checked;
+        }
+
         private void buttonGo_Click(object sender, EventArgs e)
         {
             SearchAndReplaceParameters searchAndReplaceParamenters = GetSearchAndReplaceParameters();
 
             SearchAndReplace.SearchAndReplaceInFile(searchAndReplaceParamenters);
+        }
+
+        private void textBoxFileIn_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void textBoxFileIn_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            textBoxFileIn.Text = files[0];
+        }
+
+        private void textBoxFileSearchAndReplace_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void textBoxFileSearchAndReplace_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            textBoxFileSearchAndReplace.Text = files[0];
+        }
+
+        private void radioButtonSearchAndReplaceFile_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+
+        private void radioButtonSearchAndReplaceSet_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+
+        private void buttonWindowsExplorerIntegration_Click(object sender, EventArgs e)
+        {
+            FormWindowsExplorerIntegration form = new FormWindowsExplorerIntegration();
+            form.ShowDialog();
         }
     }
 }
---
+++ JSearchAndReplace/JSearchAndReplace/FormWindowsExplorerIntegration.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JSearchAndReplace
+{
+    public partial class FormWindowsExplorerIntegration : Form
+    {
+        public FormWindowsExplorerIntegration()
+        {
+            InitializeComponent();
+        }
+
+        private void FormWindowsExplorerIntegration_Load(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+
+        private void SetRegistry()
+        {
+
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            SetRegistry();
+        }
+
+        private void UpdateUI()
+        {
+            groupBoxFiles.Enabled = checkBoxEnableRightClick.Checked;
+            groupBoxSearchAndReplace.Enabled = checkBoxEnableRightClick.Checked;
+
+            textBoxFileExtensions.Enabled = radioButtonFileExtensions.Checked;
+        }
+
+        private void checkBoxEnableRightClick_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+
+        private void radioButtonAllFiles_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+
+        private void radioButtonFileExtensions_CheckedChanged(object sender, EventArgs e)
+        {
+            UpdateUI();
+        }
+    }
+}
